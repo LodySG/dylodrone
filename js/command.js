9ca671bc0138283cb75b5279a2c4be1e25d3ffbf
lodysg <lodysg@gmail.com>
2016-12-29 15:15:10 +0100

    final

--- js/command.js
+++ js/command.js
@@ -9,21 +9,19 @@ $(function(){
 
     var log = (log) => {console.log(log)};
 
-    var roll = 1500;
-    var pitch = 1500;
-    var yaw = 1500;
-    var throttle = 1000;
-    var currentThrottle = 1000;
-    var aux = [1000, 1100, 1000, 1000];
-
-    var rollpitchmax = 150;
-    var rollpitchmin = -150;
-
-    var seuilyaw = 0.050;
-
     var mid = 1500;
     var min = 1000;
     var max = 2000;
+    var seuil = 30;
+    var maxPixVar = 150;
+    var minPixVar = 0 - maxPixVar;
+
+    var roll = mid;
+    var pitch = mid;
+    var yaw = mid;
+    var throttle = min;
+    var currentThrottle = min;
+    var aux = [min, 1100, min, min];
 
     var initialTPPos = null;
     var initialRYPos = null;
@@ -32,7 +30,7 @@ $(function(){
         return 'ontouchstart' in window  || navigator.maxTouchPoints;
     }
 
-    var socket = io();
+    // var socket = io();
 
     nx.colorize("fill", nx.randomColor());
     nx.colorize("border", nx.randomColor());
@@ -40,48 +38,78 @@ $(function(){
 
     setTimeout(function() {
 
-        throttlepitch.on("*", (data) => {            
+        throttleroll.on("*", (data) => {            
             if(data.press == 1)
                 initialTPPos = {x: data.x, y: data.y};
             
             if(data.press == 0){
                 initialTPPos = null;
                 currentThrottle = throttle;
-                pitch = 1500;
-                log({throttle: throttle,pitch: pitch});
+                roll = mid;
+                //log({throttle: throttle,pitch: pitch});
             }
 
             if(initialTPPos !== null)
             {
-                var tempX = nx.clip((data.x - initialTPPos.x),-150,150);
-                var tempY = nx.clip((initialTPPos.y - data.y),-150,150);
+                var tempX1 = data.x - initialTPPos.x;
+                var tempY1 = initialTPPos.y - data.y;
+                
+                //log({tempX1: tempX1, tempY1: tempY1});
+                
+                if(tempX1 < seuil && tempX1 > -seuil)
+                    tempX1 = 0;
+                else{
+                    tempX1 = tempX1 >= 0 ? (tempX1-seuil) : (tempX1+seuil)
+                }
 
-                throttletemp = nx.prune(nx.scale(tempY, -150, 150, (0-(min/2)),(min/2)));
-                log(throttletemp);
+                if(tempY1 < seuil && tempY1 > -seuil)
+                    tempY1 = 0;
+                else{
+                    tempY1 = tempY1 >= 0 ? (tempY1-seuil) : (tempY1+seuil)
+                }
+
+                //log({tempX1After: tempX1, tempY1After: tempY1});
+                tempX1 = nx.clip(tempX1,-maxPixVar,maxPixVar);
+                tempY1 = nx.clip(tempY1,-maxPixVar,maxPixVar);
+                throttletemp = nx.prune(nx.scale(tempY1, -maxPixVar, maxPixVar, -min,min));
                 throttle = nx.clip(currentThrottle + throttletemp, min, max);
-                pitch = nx.prune(nx.scale(tempX, -150, 150, min, max));
-                log({throttle: throttle,pitch: pitch});
+                roll = nx.prune(nx.scale(tempX1, -maxPixVar, maxPixVar, min, max));
+                //log({throttle: throttle,roll: roll});
             }
         });
 
-        rollyaw.on("*", (data) => {            
+        pitchyaw.on("*", (data) => {            
             if(data.press == 1)
                 initialRYPos = {x: data.x, y: data.y};
             
             if(data.press == 0){
                 initialRYPos = null;
-                roll = 1500;
-                pitch = 1500;
+                pitch = mid;
+                yaw = mid;
             }
 
             if(initialRYPos !== null)
             {
-                var tempX = nx.clip((data.x - initialRYPos.x),-150,150);
-                var tempY = nx.clip((initialRYPos.y - data.y),-150,150);
+                var tempX2 = data.x - initialRYPos.x;
+                var tempY2 = initialRYPos.y - data.y;
+                
+                if(tempX2 < seuil && tempX2 > -seuil)
+                    tempX2 = 0;
+                else{
+                    tempX2 = tempX2 >= 0 ? (tempX2-seuil) : (tempX2+seuil)
+                }
 
-                roll = nx.prune(nx.scale(tempX, -150, 150, min, max));
-                pitch = nx.prune(nx.scale(tempY, -150, 150, min, max));
-                log({roll: roll,pitch: pitch});
+                if(tempY2 < seuil && tempY2 > -seuil)
+                    tempY2 = 0;
+                else{
+                    tempY2 = tempY2 >= 0 ? (tempY2-seuil) : (tempY2+seuil)
+                }
+
+                tempX2 = nx.clip(tempX2,-maxPixVar,maxPixVar);
+                tempY2 = nx.clip(tempY2,-maxPixVar,maxPixVar);
+                yaw = nx.prune(nx.scale(tempX2, -maxPixVar, maxPixVar, min, max));
+                pitch = nx.prune(nx.scale(tempY2, -maxPixVar, maxPixVar, min, max));
+                //log({roll: roll,yaw: yaw});
             }
         });
 
@@ -117,25 +145,10 @@ $(function(){
             throttle: throttle,
             aux: aux
         };
-
-        socket.emit('command', controller);
-
-        //console.log(controller);
-    },100);
-    
-    setInterval(() => {
-
-        var controller = {
-            roll: roll,
-            pitch: pitch,
-            yaw: yaw,
-            throttle: throttle,
-            aux: aux
-        };
-
-        log(controller);
+        $("#controller_status").html("roll = "+roll+", pitch = "+pitch+", yaw = "+yaw+", throttle = "+throttle+", aux1 = "+aux[0]+", aux2 = "+aux[1]+", aux3 = "+aux[2]+", aux4 = "+aux[3])
+        //socket.emit('command', controller);
 
         //console.log(controller);
-    },3000);
+    },50);
 
 });
--- webserver.js
+++ webserver.js
@@ -1,18 +1,18 @@
 var express = require('express');
-var path = require('path');
+// var path = require('path');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var last_action_date = Date.now();
 var isControlled = false;
 
-var redis = require("redis"),
-    client = redis.createClient();
+// var redis = require("redis"),
+//     client = redis.createClient();
 var status = null;
 
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
+// client.on("error", function (err) {
+//     console.log("Error " + err);
+// });
 
 var portWeb = 8090;
 
@@ -25,22 +25,22 @@ app.get('/', function(req, res) {
 
 io.sockets.on('connection', (socket) => {
     socket.on('command', (data) => {
-        client.hset("drone:controller", "roll", parseInt(data.roll));
-        client.hset("drone:controller", "pitch", parseInt(data.pitch));
-        client.hset("drone:controller", "yaw", parseInt(data.yaw));
-        client.hset("drone:controller", "throttle", parseInt(data.throttle));
-        client.hset("drone:controller", "aux1", parseInt(data.aux[0]));
-        client.hset("drone:controller", "aux2", parseInt(data.aux[1]));
-        client.hset("drone:controller", "aux3", parseInt(data.aux[2]));
-        client.hset("drone:controller", "aux4", parseInt(data.aux[3]));
+        // client.hset("drone:controller", "roll", parseInt(data.roll));
+        // client.hset("drone:controller", "pitch", parseInt(data.pitch));
+        // client.hset("drone:controller", "yaw", parseInt(data.yaw));
+        // client.hset("drone:controller", "throttle", parseInt(data.throttle));
+        // client.hset("drone:controller", "aux1", parseInt(data.aux[0]));
+        // client.hset("drone:controller", "aux2", parseInt(data.aux[1]));
+        // client.hset("drone:controller", "aux3", parseInt(data.aux[2]));
+        // client.hset("drone:controller", "aux4", parseInt(data.aux[3]));
     });
 });
 
 setInterval(() => {
-    client.get("drone:status", function(err, reply) {
-        // reply is null when the key is missing 
-        status = reply;
-    });
+    // client.get("drone:status", function(err, reply) {
+    //     // reply is null when the key is missing 
+    //     status = reply;
+    // });
 },1000);
 
 server.listen(portWeb, () => console.log("app launched -> localhost:" + portWeb));
